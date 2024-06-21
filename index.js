059858b76b9587d19c8dc89bf978e5c0acdac9aa
Abdulla K A <abdulla.kaliparambil@gmail.com>
2024-06-21 19:01:28 +0530

    Node Exam

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ server.use(router)
 const PORT = 3000 || process.env.PORT
 
 server.listen(PORT,()=>{
-    console.log(`Projects Fairs Server started at port : ${PORT}`);
+    console.log(`User Server started at port : ${PORT}`);
 })
 
 server.get('/',(req,res)=>{
